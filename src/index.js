3bcb4c206c8f1239868c6122980c00eb513c6eab
hefeng6500 <2443992009@qq.com>
2020-11-23 22:12:07 +0800

    feat: 使用 context 提供者和消费者

--- src/index.js
+++ src/index.js
@@ -4,49 +4,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const UserContext = React.createContext({
+  user: "hefeng6500",
+  avatarSize: "large",
+});
+
 function Link(props) {
   return (
-    <div>
-      <a>a标签</a>
-      <p>{props.children}</p>
-    </div>
+    <UserContext.Consumer>
+      {({ user }) => {
+        return (
+          <div>
+            {user}
+            <div>{props.children}</div>
+          </div>
+        );
+      }}
+    </UserContext.Consumer>
   );
 }
 
-function Avatar(props) {
-  return <p>Avatar: {props.avatarSize}</p>;
+function Avatar() {
+  return (
+    <UserContext.Consumer>
+      {({ avatarSize }) => {
+        return <div>{avatarSize}</div>;
+      }}
+    </UserContext.Consumer>
+  );
 }
 
 function NavigationBar(props) {
   return (
-    <div>
-      {props.topBar}
-      {props.children}
-    </div>
+    <Link>
+      <Avatar />
+    </Link>
   );
 }
 
 function PageLayout(props) {
-  return <NavigationBar topBar={props.topBar} />;
+  return <NavigationBar />;
 }
 
-function Page(props) {
-  const { user, avatarSize } = props;
-  const topBar = (
-    <NavigationBar>
-      <Link user={user}>
-        <Avatar avatarSize={avatarSize} />
-      </Link>
-    </NavigationBar>
-  );
-  return <PageLayout topBar={topBar} />;
+function Page() {
+  return <PageLayout />;
 }
 
 class App extends React.Component {
   render() {
     const user = "hefeng6500";
     const avatarSize = "large";
-    return <Page user={user} avatarSize={avatarSize} />;
+    return (
+      <UserContext.Provider value={{ user, avatarSize }}>
+        <Page />
+      </UserContext.Provider>
+    );
   }
 }
 
