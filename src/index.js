6e161882a58d79baddb97324e81b87b92e9b7431
hefeng6500 <2443992009@qq.com>
2020-11-23 20:44:51 +0800

    feat: 升级了react 至 v17

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import React from "./lib/react";
-import ReactDOM from "./lib/react-dom";
+// import React from "./lib/react";
+// import ReactDOM from "./lib/react-dom";
 
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const ThemeContext = React.createContext("light");
 
 class Button extends React.Component {
+  // static contextType = ThemeContext;
   render() {
-    return <button theme={this.props.theme}>{this.props.theme}</button>;
+    return <button theme={this.context}>{this.context} </button>;
   }
 }
 
+Button.contextType = ThemeContext
+
 class ThemedButton extends React.Component {
   render() {
-    return <Button theme={this.props.theme} />;
+    return <Button />;
   }
 }
 
 function Toolbar(props) {
-  // Toolbar 组件接受一个额外的“theme”属性，然后传递给 ThemedButton 组件。
-  // 如果应用中每一个单独的按钮都需要知道 theme 的值，这会是件很麻烦的事，
-  // 因为必须将这个值层层传递所有组件。
   return (
     <div>
-      <ThemedButton theme={props.theme} />
+      <ThemedButton />
     </div>
   );
 }
 class App extends React.Component {
   render() {
-    return <Toolbar theme="dark" />;
+    return (
+      <ThemeContext.Provider value="dark">
+        <Toolbar />
+      </ThemeContext.Provider>
+    );
   }
 }
 
