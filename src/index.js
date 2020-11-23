089bfb1dc98de9689e4f126a37cbe57f91ab794a
hefeng6500 <2443992009@qq.com>
2020-11-23 21:41:12 +0800

    feat: 不使用 context API的高级写法
    直接把组件往子组件传递

--- src/index.js
+++ src/index.js
@@ -4,37 +4,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const ThemeContext = React.createContext("light");
-
-class Button extends React.Component {
-  // static contextType = ThemeContext;
-  render() {
-    return <button theme={this.context}>{this.context} </button>;
-  }
-}
-
-Button.contextType = ThemeContext
-
-class ThemedButton extends React.Component {
-  render() {
-    return <Button />;
-  }
-}
-
-function Toolbar(props) {
+function Link(props) {
   return (
     <div>
-      <ThemedButton />
+      <a>a标签</a>
+      <p>{props.children}</p>
     </div>
   );
 }
+
+function Avatar(props) {
+  return <p>Avatar: {props.avatarSize}</p>;
+}
+
+function NavigationBar(props) {
+  return (
+    <div>
+      {props.topBar}
+      {props.children}
+    </div>
+  );
+}
+
+function PageLayout(props) {
+  return <NavigationBar topBar={props.topBar} />;
+}
+
+function Page(props) {
+  const { user, avatarSize } = props;
+  const topBar = (
+    <NavigationBar>
+      <Link user={user}>
+        <Avatar avatarSize={avatarSize} />
+      </Link>
+    </NavigationBar>
+  );
+  return <PageLayout topBar={topBar} />;
+}
+
 class App extends React.Component {
   render() {
-    return (
-      <ThemeContext.Provider value="dark">
-        <Toolbar />
-      </ThemeContext.Provider>
-    );
+    const user = "hefeng6500";
+    const avatarSize = "large";
+    return <Page user={user} avatarSize={avatarSize} />;
   }
 }
 
