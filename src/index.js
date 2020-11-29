e53c86178a95a682b1bb28405b0f3e51050a3163
hefeng6500 <2443992009@qq.com>
2020-11-29 11:44:18 +0800

    feat: HOC

--- src/index.js
+++ src/index.js
@@ -14,43 +14,33 @@ let Text = function (props) {
   );
 };
 
-class MouseTracker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      x: 0,
-      y: 0,
+function WithMouseTracker(OlcComponent) {
+  return class MouseTracker extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        x: 0,
+        y: 0,
+      };
+    }
+
+    mouseMove = (event) => {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY,
+      });
     };
-  }
 
-  mouseMove = (event) => {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY,
-    });
+    render() {
+      return (
+        <div onMouseMove={this.mouseMove} style={{ border: "1px solid red" }}>
+          <OlcComponent {...this.state} />
+        </div>
+      );
+    }
   };
-
-  render() {
-    return (
-      <div onMouseMove={this.mouseMove} style={{ border: "1px solid red" }}>
-        {this.props.render(this.state)}
-      </div>
-    );
-  }
 }
 
+const HightOrder = WithMouseTracker(Text);
 
-
-ReactDOM.render(
-  <MouseTracker render={(props) => {
-    return (
-      <>
-        <h1>移动鼠标</h1>
-        <h1>X: {props.x}</h1>
-        <h1>Y: {props.y}</h1>
-      </>
-    );
-  }}>
-  </MouseTracker>,
-  document.getElementById("root")
-);
+ReactDOM.render(<HightOrder />, document.getElementById("root"));
